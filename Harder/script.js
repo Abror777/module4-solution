6120862882239f262d21923f009349fa71ea0349
Abror777 <67406504+Abror777@users.noreply.github.com>
2020-07-16 09:45:53 +0500

    Add files via upload

---
+++ Harder/script.js
@@ -0,0 +1,13 @@
+(function (window) {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+var firstLetter = names[i].charAt(0).toLowerCase();
+if (firstLetter == 'j') {
+    window.byeSpeaker.speak(names[i]);
+  } else {
+    window.helloSpeaker.speak(names[i]);
+  }
+}
+
+})(window);
